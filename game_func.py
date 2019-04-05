99d7a0b25f98e8ff083308cdc63626735ce7c6e4
IkarGhosts <ikarghosts@hotmail.com>
2019-04-05 17:01:05 +0700

    2.2 day
    организовано полностью перемещение корабля и стрельба (в стрельбе
    необходимо организовать удаление пуль вылетевших за экран и ограничить
    число одновременных выстрелов для экономии памяти приложением)

--- game_func.py
+++ game_func.py
@@ -3,13 +3,19 @@ import sys
 
 import pygame
 
+from bullet import Bullet
 
-def check_keydown_events(event, ship):
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
 
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+
+    elif event.key == pygame.K_SPACE:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -25,7 +31,7 @@ def check_events(ai_settings, screen, ship, bullets):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
 
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
@@ -34,5 +40,7 @@ def check_events(ai_settings, screen, ship, bullets):
 def update_screen(ai_settings, screen, ship, bullets):
 
     screen.fill(ai_settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
     pygame.display.flip()
