9ce842230e64106ca206fb26bb3126a7a41f2956
IkarGhosts <ikarghosts@hotmail.com>
2019-04-05 09:59:53 +0700

    first day
    Определился с проектом для изучения pygame библиотеки. Начал работу.

---
+++ start.py
@@ -0,0 +1,28 @@
+import sys
+
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+
+import game_func as gf
+
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_heigth)
+    )
+    pygame.display.set_caption("Old Galaxy")
+    ship = Ship(screen)
+
+    while True:
+
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+
+
+run_game()
